a84d86e9b70a2d1b0a3dbe07c9cba14f74f62b01
AnaPicller01 <164193234+AnaPicller01@users.noreply.github.com>
2025-04-15 08:27:10 -0300

    15/04

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lita')
+    })
+
+
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
@@ -13,8 +24,4 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
-
-
-
-
 });
